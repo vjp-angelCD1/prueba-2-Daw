69cd3f280ecb31f34dd9c56fb4d6864507166458
angelCD1 <aaclemented01@informatica.iesvalledeljerteplasencia.es>
2025-10-15 18:30:48 +0200

    gu

---
+++ 401server.php
@@ -0,0 +1,26 @@
+<?php
+echo "<h2>Contenido completo de \$_SERVER:</h2>";
+echo "<pre>";
+print_r($_SERVER);
+echo "</pre>";
+
+echo "<hr>";
+
+echo "<h2>Parámetros recibidos por GET:</h2>";
+echo "<pre>";
+print_r($_GET);
+echo "</pre>";
+
+echo "<h2>Parámetros recibidos por POST:</h2>";
+echo "<pre>";
+print_r($_POST);
+echo "</pre>";
+
+echo "<hr>";
+
+if (isset($_SERVER['HTTP_REFERER'])) {
+    echo "<p><strong>HTTP_REFERER:</strong> " . $_SERVER['HTTP_REFERER'] . "</p>";
+} else {
+    echo "<p><strong>HTTP_REFERER:</strong> (no existe, acceso directo)</p>";
+}
+?>
